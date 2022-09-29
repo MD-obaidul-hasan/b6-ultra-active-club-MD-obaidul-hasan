35c786d0721b60aabbe664d16984c153cacb9e41
MD-obaidul-hasan <md.obaidulhasanlimu@gmail.com>
2022-09-29 15:39:56 +0600

    add activity button

--- src/components/Equip/Chart/Chart.js
+++ src/components/Equip/Chart/Chart.js
@@ -3,39 +3,39 @@ import './Chart.css'
 
 const Chart = ({chart}) => {
     return (
-        <div className='chart'>
+    <div className='chart'>
             <h3>Md.Obaidul Hasan</h3>
-            <a href="">weight</a>
-            <a href="">height</a>
-            <a href="">age</a>
-<div>
-        
-    <h2>Add a break</h2>
-    
-    {/* <button onClick={() => props.handleAddChart(time)} className='btnitem'>
-                <p>20</p>
-            </button> */}
-            <div>
-            <p>Break time</p>
-            <div className='btnitems'>
-           
-                <button  ><p>15</p>
-                </button>
-                <button  ><p>25</p>
-                 </button>
-                <button  ><p>30</p>
-                </button>
-                <button  ><p>20</p>
-                </button>
-           </div>
+            <div className='charts'>
+                <p href="">weight:66kg</p>
+                <p href="">height:5.6</p>
+                <p href="">age:28</p>
             </div>
-           
-    {/* <a href="">20</a><a href="">15</a><a href="">25</a><a href="">30</a> */}
+            <div>
+               <h3>Exersise Details</h3>
+               <p>Exersise Time: {chart} </p>
+               <p>Break Time:</p>
+            </div>    
+        <div>   
+            <h2>Add a break</h2>
+    
+            <p>Break time</p>
+                <div className='btnitems'>
+                  <button  ><p>15</p></button>
+                  <button  ><p>25</p></button>
+                  <button  ><p>30</p></button>
+                  <button  ><p>20</p></button>
+                </div> 
+            <button className='acbtn'>Activity Compleate</button>    
+        </div>
+        <div>
+        
+        </div>
+        
+        
+        
+    
     </div>
-
-    <p>Exersise Time: {chart} </p>
-        <p>Break Time:</p>
-     </div>
+     
     );
 };
 
